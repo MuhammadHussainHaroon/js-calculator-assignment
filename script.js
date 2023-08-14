14d582588d29a67804a3def45d8ebaa0dbe4b3bc
muhammadhussainharoon <hussain356q@gmail.com>
2023-08-14 12:57:47 +0500

    practice

---
+++ script.js
@@ -0,0 +1,439 @@
+// let num1 = +prompt ("enter num1")
+// let num2 = +prompt ("enter num2")
+// let operator = prompt ("enter operator")
+
+// switch (operator) {
+//     case("+");
+//     let sum = num1 + num2;
+//     console.log(sum);
+//     break
+//     case("-");
+//     let sum = num1 - num2;
+//     console.log(sum);
+//     break
+//     case("*");
+//     let sum = num1 * num2;
+//     console.log(sum);
+//     break
+//     case("/");
+//     let sum = num1 / num2;
+//     console.log(sum);
+//     break
+//     case("%");
+//     let sum = num1 % num2;
+//     console.log(sum);
+//     break
+// }
+
+// function sumofferoz (y,k){
+//     let sum = y - k;
+//     console.log(sum);
+// }
+// sumof 
+
+
+
+// let num1 = +prompt ("enter your math marks")
+// let num2 = +prompt ("enter your physics marks")
+// let obtainmarks = (num1 + num2);
+// let totalmarks = (200);
+// percentage = (obtainmarks*100/totalmarks);
+// console.log ("hussain" , "tumhare" , "obtainmarks" , obtainmarks + "ha + or + tumhare" + "percentage" + percentage , "%" + "ha"); 
+
+
+
+//let alert = prompt ("you human then click");
+
+
+// let num1 = +prompt("enter your number");
+// let num2 = +prompt("enter your number");
+// let operator = prompt("enter your operator");
+// if (operator == "+") {
+//     let sum = num1 + num2
+//     console.log(sum);
+// }
+// else if (operator == "-") {
+//     let subtraction = num1 - num2
+//     console.log(subtraction);
+// }
+// else if (operator == "*") {
+//     let multiplication = num1 * num2
+//     console.log(multiplication);
+// } 
+// else if (operator == "/") {
+//     let division = num1 / num2
+//     console.log(division);
+// }
+//  else if (operator == "%") {
+//     let modulous = num1 % num2
+//     console.log(modulous);
+// }
+
+
+
+
+
+// let num1 = +prompt ("enter your number")
+// let num2 = +prompt ("enter your number")
+// let operator = prompt ("enter your operator")
+// switch (operator) {
+//     case("+"):
+//     let  sum = num1 + num2; 
+//     console.log(sum);
+//     break
+//         case("/"):
+//         let  div = num1 / num2;
+//         console.log(div);
+//         break
+//     case("-"):
+//     let sub = num1 - num2;
+//     console.log(sub);
+//     break
+//         case("*"):
+//         let mul = num1 * num2; 
+//         console.log(mul);
+//         break
+//             case("%"):
+//             let mod = num1 % num2; 
+//             console.log(mod);
+//         }
+
+// var num1 = 10
+// var num2 = 20
+// var num3 = 30
+// console.log(num3 - num2 + num1);
+
+
+// var num1 = 10 + 20
+// var num2 = 30 + 40
+// console.log(num1 + num2);
+
+
+
+
+
+
+
+
+
+
+// let y = 10
+// let z = 20
+// function oftwonumber (y,z) {
+//     if(typeof(i)== "number" && typeof(h)== "number");
+// let sum = (y+z);
+// console.log(sum);}
+
+
+
+
+// function sumoftwonumber (a , b){
+//      if(typeof (a) =="number" && typeof(b) =="number"){
+//          let sum = a + b;
+//          console.log(sum);
+//         }
+//         else{
+//             console.log('number ghalat hai');
+//         }
+//     }
+//     sumoftwonumber(10 ,20);
+    
+    
+    
+    // function sumOfTwoNumber (i , h){
+        //         if(typeof(i) == "number" && typeof(h) == "number"){
+            //              let sum = i + h;
+            //              console.log(sum);
+            //          }
+            //          else{
+                //              console.log("bhaijan number bhej");
+//          }
+//      }
+
+//      sumOfTwoNumber(50 , 40);
+    
+    
+
+
+
+
+// function hussain (a , b , c , d){
+// if(typeof(a)=="number" && typeof(b)=="number"  && typeof(c)=="number" && typeof(d)=="number"){
+//     let sub = a-b-c+d
+//     return sub
+// }
+// else{
+//     console.log("bhaijan number bhej");
+// }
+// }
+// const sub = hussain (150 , 100 , 50 , 80)
+// console.log(sub);
+
+
+
+
+// function ramokaka (dish1 , dish2 , dish3){
+//    console.log("bhaijan apka order" + "jisme" + dish1 + dish2 + dish3 + "ha");
+
+// }
+// ramokaka ("biryani" , "pilao" , "pepsi");
+
+// function istekhara (name , mothername , age , adress){
+//     if(name=="feroz" && mothername=="khala" && age=="20" && adress=="kharader"){
+//     console.log(name + ""+"beta ache hai");
+//     }
+//     else{
+//         console.log(name + "" + "beta is qabil nahi");
+//     }
+
+// }
+// istekhara("feroz" , "khala" , 22 , "kharader" )
+
+
+
+
+
+
+
+
+
+// let a = 20
+// a = 10
+// console.log(a);
+
+
+
+// const a = 20
+// console.log(a);
+
+
+// let a = 10
+// let b = 20
+// console.log(a+ b);
+
+
+// let names = prompt ("enter your name");
+// let math =+prompt ("enter your math marks");
+// let english =+prompt ("enter your english marks");
+// let urdu =+prompt ("enter your urdu marks");
+// let obtainmarks = (math + english + urdu);
+// let totalmarks = (300);
+// let percentage = (obtainmarks*100/totalmarks);
+// console.log("ap ka name" + names + "ha" + "or ap ki percentage" + percentage + "%" + "hai");
+
+
+// let num1 = +prompt ("enter your number");
+// let num2 =+prompt ("enter your number");
+// let operator = prompt ("enter your operator");
+// if(operator=="+"){
+//     let sum = num1+num2;
+//     console.log(sum);
+// }
+// if(operator=="-"){
+//     let sum = num1-num2;
+//     console.log(sum);
+// }
+// if(operator=="*"){
+//     let sum = num1*num2;
+//     console.log(sum);
+// }
+// if(operator=="/"){
+//     let sum = num1/num2;
+//     console.log(sum);
+// }
+// if(operator=="%"){
+//     let sum = num1%num2;
+//     console.log(sum);
+// }
+
+
+// let num1 = +prompt ("enter your number");
+// let num2 =+prompt ("enter your number");
+// let operator = prompt ("enter your operator");
+// switch (operator){
+//     case  ("+"):
+//     let sum = num1 + num2;
+//     console.log(sum);
+//     break
+//     case ("-"):
+//     let sub = num1 - num2;
+//     console.log(sub);
+//     break
+//     case ("*"):
+//     let mul= num1 * num2;
+//     console.log(mul);
+//     break
+//     case ("/"):
+//     let a = num1 / num2;
+//     console.log(a);
+//     break
+//     case ("%"):
+//     let mod = num1 % num2;
+//     console.log(mod);
+//     break
+// }
+
+
+
+
+
+// function istekhara (name , salary , house , bankbalance) {
+//     if(salary==40000 && house==true && bankbalance >= 50000){
+//        return(true)
+//     }
+//     else{
+//         return(false)
+//     }
+// }
+// const a = istekhara ("babo",40000,true,80000)
+// console.log(a);
+
+
+
+// function ramokaka (dish1 , dish2 , dish3 , dish4 , dish5){
+//     if(dish1=="BIRYANI" && dish2=="KHATAMHOGAE" && dish3=="KALANA" && dish4=="MEHANGIHAI" && dish5=="PEPSI"){
+// return(true)   
+//  }
+//     else{
+//         return(false)
+//     }
+//     }
+// let a = ramokaka ("BIRYANI" , "KHATAMHOGAE" , "KALANA" , "MEHANGIHAI" , "PEPSI")
+//     console.log(a);
+    
+
+
+
+// function ramokaka (dish1 , dish2 , dish3 , dish4 , dish5){
+// return(ramokaka)
+
+// }
+
+
+// let a = ramokaka ("biryani" , "KHATAMHOGAE" , "KALANA" , "MEHANGIHAI" , "PEPSI")
+//  console.log(a +"hai");
+
+
+//  var num1 = 20;
+//   a = num1 + 30;
+//   console.log(a);
+//   num1 = a *= 30;
+//   console.log(num1);
+
+
+
+
+
+// var num1 = 10
+// var num2 = 10
+// console.log(num1>num2);
+// console.log(num1==num2);
+// console.log(num1!=num2);
+
+// var names =prompt("enter your number");
+// console.log(typeof(names));
+// console.log(names);
+
+
+//alert("enter your names")
+
+//var a = (3*6) * (9+2);
+//console.log(a);
+
+// var a = +prompt("enter your math marks")
+// var b = +prompt("enter your math marks")
+// var c = +prompt("enter your math marks")
+// var d = +prompt("enter your math marks")
+// var totalmarks = (400)
+// var obtainmarks = (a+b+c+d)
+// var percentage = (obtainmarks*100/totalmarks)
+// if(percentage>=80){
+//     console.log("your grade is A+");
+// }
+// else if(percentage>=60){
+//     console.log("your grade is A");
+// }
+// else if(percentage>=50){
+//     console.log("your grade is B");
+// }
+// else {
+//     console.log("fail");
+// }
+
+// var checkNum = 10 > 5 && 10 < 5 || 15 > 10;
+// console.log(checkNum);
+
+// var checkNum = 10 != 10;
+//  var checkNum2 = 10 !== 10;
+//  console.log(checkNum);
+//  console.log(checkNum2);
+
+
+// let mynames = "hussain";
+// let names = prompt ("enter your name");
+// if(mynames==names){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+
+// let age = 23
+// if(age>18){
+//     console.log("old");
+// }
+// if(age>19){
+//     console.log("older");
+// }
+// else{
+//     console.log(young);
+// }
+
+
+// let a = 23 
+// let b = 24
+// let sum = a + b
+// console.log(typeof(b));
+
+
+// const heading01 = document.querySelector(".hello css");
+// heading01.innerhtml = "hello css"
+// console.log(heading01);
+
+ const h1 = document.querySelector(".first-head");
+ h1.innerHTML = "Hello <br/> hussain";
+ console.log(h1);
+
+
+
+const h3 = document.querySelector(".second-head");
+h3.innerhtml = "hello <br/> hassan";
+console.log(h3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
